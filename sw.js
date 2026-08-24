6eda94091185227dab00012c23fb7210dbb68b08
fmuniru-gif <fmuniru@gmail.com>
2026-08-24 09:09:44 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* ZEZMS Owner Edition v3.16.2 - Ledger Integrity Stabilization r53A */
-const CACHE = 'zezms-ledger-integrity-20260823-r53a';
-const PATCHED_INDEX_CACHE = 'zezms-r53a-baseline-cache-v2';
+/* ZEZMS Owner Edition v3.16.2 - Receipt Integrity Stabilization r53B */
+const CACHE = 'zezms-receipt-integrity-20260824-r53b';
+const PATCHED_INDEX_CACHE = 'zezms-r53b-baseline-cache-v1';
 const ASSETS = [
   './',
   './index.html',
